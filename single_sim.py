92bfd16e8f4f666e5555c9a8c889deeb5b05776f
thoulden <101445737+thoulden@users.noreply.github.com>
2024-12-16 16:53:26 +0000

    Update single_sim.py

--- single_sim.py
+++ single_sim.py
@@ -11,8 +11,8 @@ def run():
     compute_growth = st.sidebar.checkbox('Compute Growth', value=True)
 
     # Parameters for the simulation
-    lambda_sample = st.sidebar.number_input('Parallelizability (λ)', min_value=0.01, max_value=1.0, value=0.7, step=0.01)
-    r_0_sample = st.sidebar.number_input('Initial Research Productivity (r₀)', min_value=0.0, max_value=5.0, value=0.7, step=0.1)
+    lambda_sample = st.sidebar.number_input('Parallelizability (λ)', min_value=0.01, max_value=1.0, value=0.4, step=0.01)
+    r_0_sample = st.sidebar.number_input('Initial Research Productivity (r₀)', min_value=0.0, max_value=5.0, value=1.2, step=0.1)
     Yr_Left_sample = st.sidebar.number_input('Years Till Ceiling', min_value=1.0, max_value=50.0, value=9.0, step=0.5)
     f_sample = st.sidebar.number_input('Acceleration term (f)', min_value=1.0, max_value=100.0, value=8.0, step=0.1)
 
