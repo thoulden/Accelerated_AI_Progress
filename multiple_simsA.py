0239c1d1b1c2d5d40ea0f1f76712108aceed2f95
thoulden <101445737+thoulden@users.noreply.github.com>
2024-12-18 14:54:47 +0000

    Update multiple_simsA.py

--- multiple_simsA.py
+++ multiple_simsA.py
@@ -277,4 +277,7 @@ def run():
         st.write("### Fraction of Simulations Meeting Each Condition")
         st.table(formatted_df.data)
 
+    else 
+        st.markdown("Select 'run simulation' to produce results")
+
 
