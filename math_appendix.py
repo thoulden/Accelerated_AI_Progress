f8f9905cf93a5376724b781b4d58088f2149c6ff
thoulden <101445737+thoulden@users.noreply.github.com>
2024-12-22 22:11:33 +0000

    Update math_appendix.py

--- math_appendix.py
+++ math_appendix.py
@@ -11,15 +11,17 @@ def display():
     st.markdown("""##### Connection between doubling time and semi-endogenous growth models.""")
     st.markdown(r""" How do we derive the relaitonship between software doubling and software progress doubling from semi-endogenous growth models? Take the standard semi-endogenous growth environment""")
     st.latex(r"""
-    \dot{S}_t(S_t, C_t) = (R(S_t, C_t))^{\lambda} S_t^{1-\frac{\lambda}{r(S_t)}}
+    \dot{S}_t(S_t, C_t) = (R(S_t, C_t))^{\lambda} S_t^{1-\frac{\lambda}{r(S_t)}} \implies \dot{g}_{S,t}(S_t, C_t) = (R(S_t, C_t))^{\lambda} S_t^{-\frac{\lambda}{r(S_t)}}
     """)
     st.markdown(r"""In contrast to the standard semi-engodenous growth picture we replace human labor with denotes effective researchers, $R$. Since we are assuming that AI completely replaces human labor, effective researchers are just a function of software capabilities and compute.
     To combine software quality and compute availability into effective researchers we assume a Cobb-Douglass form: $R(S_t, C_t) = (a S_t)^{\alpha} (b C_t)^{1-\alpha}$. 
     Now, to answer `how much does the pace of software progress change when we double software quality (holdnding compute fixed) we just compare \dot{S}(S, C) with \dot{S}(2S, C): """)
     st.latex(r"""
-    \frac{\dot{S}(2S, C)}{\dot{S}(S, C)} = \frac{[(a2S)^{\alpha}(b C)^{1-\alpha}]^{\lambda} (2S)^{1-\frac{\lambda}{r}}}{[(aS)^{\alpha}(b C)^{1-\alpha}]^{\lambda} S^{1-\frac{\lambda}{r(2S)}}} = 2^{\lambda \alpha}\times 2^{1-\frac{\lambda}{r(2S)}}\times S^{\lambda({\frac{1}{r(S)} - \frac{1}{r(2S)}})}
+    \frac{\dot{S}(2S, C)}{\dot{S}(S, C)} = \frac{[(a2S)^{\alpha}(b C)^{1-\alpha}]^{\lambda} (2S)^{-\frac{\lambda}{r}}}{[(aS)^{\alpha}(b C)^{1-\alpha}]^{\lambda} S^{-\frac{\lambda}{r(2S)}}} = 2^{\lambda \alpha}\times 2^{-\frac{\lambda}{r(2S)}}\times S^{\lambda({\frac{1}{r(S)} - \frac{1}{r(2S)}})}
     """)
-    st.markdown(r""" to simplify this expression we just ignore the last $S$ term (i.e., assume here that ${\frac{1}{r(S)} =\frac{1}{r(2S)}}$), so that the rate of doubling is independent of the level of $S$. We can see that this assumption results in an overestimation of the impact of software doubling on the rate of software progress (since we are assuming that $r(2S) < r(S)$). """)
+    st.markdown(r""" to simplify this expression we just ignore the last $S$ term (i.e., assume here that ${\frac{1}{r(S)} =\frac{1}{r(2S)}}$), so that the rate of doubling is independent of the level of $S$. We can see that this assumption results in an overestimation of the impact of software doubling on the rate of software progress (since we are assuming that $r(2S) < r(S)$). We expect this overestimation to be relatively minor given a sufficient number of software doublings availaible before the software ceiling. 
+    Under this assumetion we arive at the result that a doubling of software doubles the rate of software progress $\lambda(\alpha - \frac{1}{r(2S)})$ times, or equivalently, by $\lambda\alpha(1 - \frac{1}{r(2S)\alpha})$ times. 
+    """)
     
     
     st.markdown("""##### Semi-Endogenous Growth Environment""")
