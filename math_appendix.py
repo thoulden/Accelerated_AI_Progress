ca97f518cb339b21a629a6a1f1258d4c0a90e9e0
thoulden <101445737+thoulden@users.noreply.github.com>
2024-12-22 21:54:10 +0000

    Update math_appendix.py

--- math_appendix.py
+++ math_appendix.py
@@ -2,6 +2,23 @@ import streamlit as st
 
 def display():
     st.markdown(r""" ## Math Appendix""")
+
+    st.markdown("""##### Retraining Costs""")
+    st.markdown(r""" In the model we assume that everytime software doubles, the pace of software progress doubles $q$ times, where $q = \lambda(r^{-1} - 1)$. The assumption underlying this process is that models can be developed through a relatively continuous process -- building on the previous model. In reality, state of the art models have to be trained, a process that takes time. To implement this possibility we assume that software improvements can also be used to increase the pace of training. 
+    The median parameters suggest that software must double roughly 5 times to double the pace of software progress, to account for retraining we assume that software has to double an extra time (so roughly 6 times) to double the pace of software progress. 
+    To implement this additional requirement, we assume that every time software doubles, the pace of software doubles $q/|q+1|$ times.""")
+
+    st.markdown("""##### Connection between doubling time and semi-endogenous growth models.""")
+    st.markdown(r""" How do we derive the relaitonship between software doubling and software progress doubling from semi-endogenous growth models? Take the standard semi-endogenous growth environment""")
+    st.latex(r"""
+    \dot{S}_t(S_t, C_t) = (R(S_t, C_t))^{\lambda} S_t^{1-\frac{\lambda}{r(S_t)}}
+    """)
+    st.markdown(r"""In contrast to the standard semi-engodenous growth picture we replace human labor with denotes effective researchers, $R$. Since we are assuming that AI completely replaces human labor, effective researchers are just a function of software capabilities and compute.
+    To combine software quality and compute availability into effective researchers we assume a Cobb-Douglass form: $R(S_t, C_t) = (a S_t)^{\alpha} (b C_t)^{1-\alpha}$. 
+    Now, to answer `how much does the pace of software progress change when we double software quality (holdnding compute fixed) we just compare \dot{S}(S, C) with \dot{S}(2S, C): """)
+    st.latex(r"""
+    \frac{\dot{S}(2S, C)}{\dot{S}(S, C)} = \frac{[(aS)^{\alpha}(b C)^{1-\alpha}]^{\lambda} S^{1-\frac{\lambda}{r}}}{[(a2S)^{\alpha}(b C)^{1-\alpha}]^{\lambda} (2S)^{1-\frac{\lambda}{r(2S)}}}
+    """)
     
     st.markdown("""##### Semi-Endogenous Growth Environment""")
     st.markdown(r""" Throughout, I assume software, $S$ follows the law of motion""")
