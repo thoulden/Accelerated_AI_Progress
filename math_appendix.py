e48c306d8e10995f6a911cb6a509660db85f7f31
thoulden <101445737+thoulden@users.noreply.github.com>
2025-01-19 18:19:15 +0000

    Update math_appendix.py

--- math_appendix.py
+++ math_appendix.py
@@ -42,20 +42,3 @@ def display():
 
   On ii), this difference can't be rectified directly with our simple model. We can see that this term comes from the fact that $r$ is declining over time as software level grows and which implies that the exponent on $S$ after a doubling of software will decrease. Because $r$ is declining, we have that $\lambda(\frac{1}{r(2S)} - \frac{1}{r(S)}) > 0$ which implies that, relative to the semi-engodenous growth model, our simple model will over estimate the doubling time according to the itterative doubling time expression above. We omit this terms since it complicates the model. We prefer for the changing in $r$ over time to be thought of as a simple way of implementing a pentaly to additional software progress, rather than raken too litterally as we in the semi-endogenous version of this model detailed above.   
   """)
-
-  
-
-  
-  
-  st.markdown(r"""Now, to answer: "How much does the pace of software progress change when we double software quality (holding compute fixed)?" we compare $g(S, C)$ with $g(2S, C)$:
-  """)
-  st.latex(r"""
-  \frac{g_{S}(2S, C)}{g_{S}(S, C)} = \frac{[(a2S)^{\alpha}(b C)^{1-\alpha}]^{\lambda} (2S)^{-\frac{\lambda}{r(2S)}}}{[(aS)^{\alpha}(b C)^{1-\alpha}]^{\lambda} S^{-\frac{\lambda}{r(S)}}} = 2^{\lambda \alpha}\times 2^{-\frac{\lambda}{r(2S)}}\times S^{\lambda({\frac{1}{r(S)} - \frac{1}{r(2S)}})}
-  """)
-  st.markdown(r"""To simplify this expression, we ignore the last $S$ term (i.e., assume here that ${\frac{1}{r(S)} =\frac{1}{r(2S)}}$), so that the change in the rate of doubling over time is only dependent on $S$ through its impact on $r$ (and not through the direct impact on software levels). 
-  This assumption results in an overestimation of the impact of software doubling on the rate of software progress (since we are assuming that $r(2S) < r(S)$). We expect this overestimation to be relatively minor, given a sufficient number of software doublings available before reaching the software ceiling.
-
-  Under this assumption, we arrive at the result that doubling software doubles the rate of software progress $\lambda(\alpha - \frac{1}{r(2S)})$ times, or equivalently, by $\lambda\alpha(1 - \frac{1}{r(2S)\alpha})$ times. In the original post, we (generally) assume that compute is held fixed. 
-
-  Instead of introducing $\alpha$ (which does affect the relationship between software levels and software progress), we opt to reduce estimates of $\lambda$ and $r$ to account for the fact that software is only one component of "effective researchers." Defining $\hat{\lambda} = \lambda \alpha$ and $\hat{r} = r \alpha$, we can see that doubling software levels doubles the rate of software growth by $\hat{\lambda}(1 - \frac{1}{\hat{r}(2S)})$, as implemented in the simulation.""")
-
