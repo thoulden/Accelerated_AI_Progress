8ab4a8dc9b1f47f364f2c2c4a66dc8f3906f6715
thoulden <101445737+thoulden@users.noreply.github.com>
2025-01-19 15:03:07 +0000

    fixing download button

--- multiple_simsA.py
+++ multiple_simsA.py
@@ -103,7 +103,7 @@ def run_simulations(num_sims, conditions, r_low, r_high, ly_low, ly_high, lf_low
 
     for i, params in enumerate(params_batch):
         r_initial, factor_increase, initial_factor_increase_time, limit_years, compute_growth_monthly_rate, f_0, f_max, lambda_factor = params
-        times, _, _, _, _ = dynamic_system_with_lambda(
+        times, sizes, rs, compute_sizes, f_values = dynamic_system_with_lambda(
          r_initial, factor_increase, initial_factor_increase_time, limit_years, compute_growth_monthly_rate, f_0, f_max, lambda_factor, retraining_cost)
         times_matrix.append(times)
         sizes_matrix.append(sizes)
