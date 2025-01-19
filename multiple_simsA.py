c7852d8485c3fbb4993339eaecf243be33d5b09f
thoulden <101445737+thoulden@users.noreply.github.com>
2025-01-19 16:35:36 +0000

    Update multiple_simsA.py

--- multiple_simsA.py
+++ multiple_simsA.py
@@ -221,7 +221,7 @@ def run():
             file_name="simulation_results.csv",
             mime="text/csv",
         )
-        st.write("Recommended for small number of simulations only.")
+        st.write("Download recommended for small number of simulations only.")
     else:
         st.write("Press 'Run Simulation' to view results.")
     
