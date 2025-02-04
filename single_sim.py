4a1404aca2fea9b71f32d304449553b6121e14d2
thoulden <101445737+thoulden@users.noreply.github.com>
2025-02-04 12:11:29 +0000

    Update single_sim.py

--- single_sim.py
+++ single_sim.py
@@ -74,8 +74,8 @@ def run():
     # Option to compute growth
     compute_growth = st.sidebar.checkbox('Compute Growth')
     if compute_growth:
-        f_sample_min = st.sidebar.number_input('Initial Acceleration (f)', min_value=0.0, max_value=1000.0, value=0.10, step=0.1)
-        f_sample_max = st.sidebar.number_input('Max Acceleration (f)', min_value=f_sample_min, max_value=1000.0, value=32.0, step=0.1)
+        f_sample_min = st.sidebar.number_input('Initial Research Boost ', min_value=0.0, max_value=1000.0, value=0.10, step=0.1)
+        f_sample_max = st.sidebar.number_input('Max Boost', min_value=f_sample_min, max_value=1000.0, value=32.0, step=0.1)
     else:
         f_sample = st.sidebar.number_input('Acceleration term (f)', min_value=0.0, max_value=1000.0, value=8.0, step=0.1)
         f_sample_max = f_sample
