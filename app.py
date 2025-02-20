9df1a086c22f44dd58d3bbeda59c48bfc68d2cf7
thoulden <101445737+thoulden@users.noreply.github.com>
2025-02-20 11:08:04 +0000

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import single_sim
 # Parameters table in Markdown format
 st.set_page_config(
     page_title="AI Progress Sim",
-    page_icon=":smiley:",  # You can also use an image file or URL
+    page_icon="🤖:",  # You can also use an image file or URL
     layout="wide",  # Other options: "centered"
     initial_sidebar_state="expanded"  # Other options: "collapsed", "auto"
 )
