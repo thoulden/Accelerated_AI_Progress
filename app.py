266a293f87e7d0df91ce4bd624cf56fa3f873d84
thoulden <101445737+thoulden@users.noreply.github.com>
2025-05-31 11:02:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -21,9 +21,10 @@ def get_parameters_table_markdown():
 | Initial Speed Up, $f$                      | After AI Systems for AI R&D Automation (ASARA) is deployed, how many times faster does software progress become (compared to the recent pace of software progress)?  | 2             | 8               | 32            |
 | $r$                       | Controls the degree of diminishing returns to research. Each time cognitive inputs to software R&D double, how many times does software double? (Note this parameter falls over time.)                | 0.4           | 1.2             | 3.6           |
 | Distance to effective limits on software   | At the start of the simulation, how far is software from effective limits? Measured in the years of AI progress at recent rates of progress.         | 5             | 9               | 13            |
-| Parallelizability of research, $\lambda$   | If cognitive inputs to software R&D instantaneously double, how many times does the pace of software progress double?                                    | 0.15          | 0.3             | 0.6           |
+| Parallelizability of research, $p$   | If cognitive inputs to software R&D instantaneously double, how many times does the pace of software progress double?                           | 0.15          | 0.3             | 0.6           |
     '''
     return table_markdown
+st.markdown(r"""Note: relative to standard calibrations of parallelizability and diminishing returns to research effort, both $r$ and $p$ are deflated by the degree of diminishing returns to cognitive labour in software research effort which we take to be 0.5.""")
 
 # Main Page content
 st.title('Simulation of Accelerated AI Progress')
@@ -69,6 +70,6 @@ In addition to model parameters, you can select whether to enable additional mod
 
 st.markdown("### Sampling")
 st.markdown(
-r"When 'Multiple Simulations' is selected, randomization occurs over log-uniform distributions for $f$, $r_0$, and $\lambda$, while the years until the ceiling are randomized over a uniform distribution. The bounds for these distributions come from the sidebar inputs."
+r"When 'Multiple Simulations' is selected, randomization occurs over log-uniform distributions for $f$, $r_0$, and $p$, while the years until the ceiling are randomized over a uniform distribution. The bounds for these distributions come from the sidebar inputs."
 )
 
